573d93124eb7dbd2386bfc5dde024c604fb830f4
fujic <fujic@fujiikousei-no-MacBook-Pro.local>
2016-01-28 01:01:27 +0900

    add Form

---
+++ AsyncForm/Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsyncForm
+{
+    //全般的には.Net3.5定義済みデリゲート型とラムダ式を使用しています。
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //Threadを使用した方法
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.button1.Text = "Waite 3Sec";
+            this.button1.Enabled = false;
+            var thr = new Thread(() =>
+            {
+                Thread.Sleep(3000);
+                this.BeginInvoke((Action)(() =>
+                {
+                    this.button1.Enabled = true;
+                    this.button1.Text = "3秒超過";
+                }));
+            });
+            thr.Start();
+        }
+        //ThreadPoolを使用した方法　
+        //パフォーマンスの観点からこちらを使用すべきである。
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.button2.Text = "Waite 3Sec";
+            this.button2.Enabled = false;
+            //ThreadPoolを利用しています。
+            //本来コールバックをラムダ式Action<object>で表現していますが
+            //引数を使用しないためアンダースコアを利用しています。
+            ThreadPool.QueueUserWorkItem(_ =>
+            {
+                Thread.Sleep(3000);
+                this.BeginInvoke((Action)(() =>
+                {
+                    this.button2.Enabled = true;
+                    this.button2.Text = "3秒超過";
+                }));
+            },null);
+            
+        }
+
+        #region .N20から有効なEAP(Event Async Pattern)
+
+        private readonly BackgroundWorker worker = new BackgroundWorker();
+        private void SetButton3Event()
+        {
+            this.worker.DoWork -= asyncDoWork;
+            this.worker.RunWorkerCompleted -= Worker_RunWorkerCompleted;
+            this.worker.DoWork += asyncDoWork;
+            this.worker.RunWorkerCompleted += Worker_RunWorkerCompleted;
+        }
+        //.Net2.0で取り込まれたBackGroundWorkerを使用します。
+        private void button3_Click(object sender, EventArgs e)
+        {
+            SetButton3Event();
+            this.button3.Enabled = false;
+            this.worker.RunWorkerAsync();//非同期処理開始　Worker_DoWork()起動
+        }
+        //UI Thread処理
+        private void Worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            var result = (string)e.Result;//結果を受け取る
+            this.button3.Text = result;
+            this.button3.Enabled = true;
+        }
+        //非同期処理
+        private void asyncDoWork(object sender, DoWorkEventArgs e)
+        {
+            Thread.Sleep(3000);
+            e.Result = "3Sec";　//結果を渡す。
+        }
+
+        #endregion
+        //ThreadPoolで非同期処理実施し戻り値が使用するパターン
+        //APM（Asynchronous Programming Model）
+        private void button4_Click(object sender, EventArgs e)
+        {
+            this.button4.Enabled = false;
+            var ayncMethod = new Func<string>(() =>
+            {
+                Thread.Sleep(3000);
+                return "3秒経過";
+            });
+            ayncMethod.BeginInvoke(ar => 
+            {
+                var res = ayncMethod.EndInvoke(ar);
+                this.BeginInvoke((Action)(()=>
+                {
+                    this.button4.Enabled = true;
+                    this.button4.Text = res;
+                }));
+            },null);
+        }
+
+        //TaskBase Asynchronous Pattern .Net4.0
+        private void button5_Click(object sender, EventArgs e)
+        {
+            button5.Enabled = false;
+            Task.Factory.StartNew(() => Thread.Sleep(3000))
+                .ContinueWith(_ => //UI Threadで処理される。引数のTaskSchedulerがみそ
+                {
+                    this.button5.Enabled = true;
+                    this.button5.Text = "経過";
+                },TaskScheduler.FromCurrentSynchronizationContext());
+        }
+        //aysnc/await for .Net4.5
+        private async  void button6_Click(object sender, EventArgs e)
+        {
+            this.button6.Text = "開始";
+            this.button6.Enabled = false;
+            await Task.Run(() => Thread.Sleep(3000));
+            this.button6.Text = "終了";
+            this.button6.Enabled = true;
+        }
+
+
+        //.Net3.5から標準ライブラリーとしてReactive Extensions（Rx）が省略
+        //Linqと同等のメソッドチェーンで記述できる。今回は割愛
+
+       
+
+    }
+}
---
+++ Net3.5AsyncConsole/Program.cs
@@ -0,0 +1,210 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading;
+
+namespace Net3._5AsyncConsole
+{
+    class Program
+    {
+        //基本ThreadPoolで処理する関数　引数を一指定できる。
+        static void BasicThreadMethod(object stat)
+        {
+            var tmpObj = (object[])stat;
+            string arg = string.Empty;
+            for (int l = 0; l < tmpObj.Length; l++)
+            {
+                arg += tmpObj[l].ToString();
+            }
+            for (int i = 0; i < 150; i++)
+            {
+                Thread.Sleep(10);
+                Console.Write(" {0}" ,arg);
+            }
+            int workThread, compThread;
+            ThreadPool.GetAvailableThreads(out workThread,out compThread);
+            Console.WriteLine("Working: {0},Completed: {1}",workThread,compThread);
+        }
+        static void Main(string[] args)
+        {
+            //ThreadPoolの基本2.0?
+            WaitCallback waitCaller = new WaitCallback(BasicThreadMethod);
+            //ThreadPoolに登録
+            ThreadPool.QueueUserWorkItem(waitCaller, new object[] { "X" });
+            ThreadPool.QueueUserWorkItem(waitCaller, new object[] { "*" });
+            //object配列を渡すと複数の引数を渡せる。
+            ThreadPool.QueueUserWorkItem(waitCaller, new object[] { "I", "-" });
+
+            int workThread, compThread;
+            ThreadPool.GetAvailableThreads(out workThread, out compThread);
+            Console.WriteLine("Working: {0},Completed: {1}", workThread, compThread);
+            Console.ReadKey();
+
+
+            //ThreadPoolを使用しています。
+            //基本的にEndInvokeを呼びスレッドを閉じなくてはならない。
+            //デリゲートを使用した方法
+            BlockingCollBack();
+            Console.ReadKey();
+            //ラムダ式を使用した方法
+
+        }
+        //EndInvokeによるブロッキング
+        static void BlockingEndInvoke()
+        {
+            // The asynchronous method puts the thread id here.
+            int threadId;
+
+            // Create an instance of the test class.
+            AsyncDemo ad = new AsyncDemo();
+
+            // Create the delegate.
+            AsyncMethodCaller caller = new AsyncMethodCaller(ad.TestMethod);
+
+            // Initiate the asychronous call.
+            // デリゲートを同じ引き数＋コールバック関数＋コールバック引数となります。
+            IAsyncResult result = caller.BeginInvoke(3000,
+                out threadId, null, null);
+
+            Thread.Sleep(0);
+            Console.WriteLine("Main thread {0} does some work.",
+                Thread.CurrentThread.ManagedThreadId);
+
+            // Call EndInvoke to wait for the asynchronous call to complete,
+            // and to retrieve the results.
+            string returnValue = caller.EndInvoke(out threadId, result);
+
+            Console.WriteLine("The call executed on thread {0}, with return value \"{1}\".",
+                threadId, returnValue);
+        }
+        //WaitHandle による非同期呼び出しの待機
+        static void BlockingWaitHandle()
+        {
+            // スレッドID保存用の変数を宣言
+            int threadId;
+
+            //AsyncDemoクラスをインスタンス化
+            AsyncDemo ad = new AsyncDemo();
+
+            //AsyncDemoのデリゲート型を使用してデリゲートを作成
+            AsyncMethodCaller caller = new AsyncMethodCaller(ad.TestMethod);
+
+            // デリゲートのBeginInvokeを使用してThreadPoolで処理を開始
+            // 引数#1はデリゲートの引数１引数#2はデリゲートの第二引数
+            // #3,コールバック関数を利用する場合、#4はコールバック関数の引数
+            IAsyncResult result = caller.BeginInvoke(3000,
+                out threadId, null, null);
+
+            Thread.Sleep(0);
+            Console.WriteLine("Main thread {0} does some work.",
+                Thread.CurrentThread.ManagedThreadId);
+
+            // Wait for the WaitHandle to become signaled.
+            result.AsyncWaitHandle.WaitOne();
+
+            // Perform additional processing here.
+            // Call EndInvoke to retrieve the results.
+            string returnValue = caller.EndInvoke(out threadId, result);
+
+            // Close the wait handle.
+            result.AsyncWaitHandle.Close();
+
+            Console.WriteLine("The call executed on thread {0}, with return value \"{1}\".",
+                threadId, returnValue);
+        }
+        //非同期呼び出し完了のポーリングによる待機
+        static void BlockingIsCompleted()
+        {
+            // The asynchronous method puts the thread id here.
+            int threadId;
+
+            // Create an instance of the test class.
+            AsyncDemo ad = new AsyncDemo();
+
+            // Create the delegate.
+            AsyncMethodCaller caller = new AsyncMethodCaller(ad.TestMethod);
+
+            // Initiate the asychronous call.
+            IAsyncResult result = caller.BeginInvoke(3000,
+                out threadId, null, null);
+
+            // Poll while simulating work.ここでポーリング確認
+            while (result.IsCompleted == false)
+            {
+                Thread.Sleep(250);
+                Console.Write(".");
+            }
+
+            // Call EndInvoke to retrieve the results.
+            string returnValue = caller.EndInvoke(out threadId, result);
+
+            Console.WriteLine("\nThe call executed on thread {0}, with return value \"{1}\".",
+                threadId, returnValue);
+        }
+        //非同期呼び出し完了じのコールバックによる待機
+        static void BlockingCollBack()
+        {
+            // Create an instance of the test class.
+            AsyncDemo ad = new AsyncDemo();
+
+            // Create the delegate.
+            AsyncMethodCaller caller = new AsyncMethodCaller(ad.TestMethod);
+
+            // The threadId parameter of TestMethod is an out parameter, so
+            // its input value is never used by TestMethod. Therefore, a dummy
+            // variable can be passed to the BeginInvoke call. If the threadId
+            // parameter were a ref parameter, it would have to be a class-
+            // level field so that it could be passed to both BeginInvoke and 
+            // EndInvoke.
+            int dummy = 0;
+
+            // Initiate the asynchronous call, passing three seconds (3000 ms)
+            // for the callDuration parameter of TestMethod; a dummy variable 
+            // for the out parameter (threadId); the callback delegate; and
+            // state information that can be retrieved by the callback method.
+            // In this case, the state information is a string that can be used
+            // to format a console message.
+            IAsyncResult result = caller.BeginInvoke(3000,
+                out dummy,
+                new AsyncCallback(CallbackMethod),
+                "The call executed on thread {0}, with return value \"{1}\".");
+
+            Console.WriteLine("The main thread {0} continues to execute...",
+                Thread.CurrentThread.ManagedThreadId);
+
+            // mainスレッドを待機させる3秒以上待機させても問題ない。
+            //Thread.Sleep(4000);
+            // Wait for the WaitHandle to become signaled.
+            result.AsyncWaitHandle.WaitOne();
+
+            Console.WriteLine("The main thread ends.");
+        }
+        // The callback method must have the same signature as the
+        // AsyncCallback delegate.
+        static void CallbackMethod(IAsyncResult ar)
+        {
+            // Retrieve the delegate.
+            AsyncResult result = (AsyncResult)ar;
+            AsyncMethodCaller caller = (AsyncMethodCaller)result.AsyncDelegate;
+
+            // Retrieve the format string that was passed as state 
+            // information.
+            string formatString = (string)ar.AsyncState;
+
+            // Define a variable to receive the value of the out parameter.
+            // If the parameter were ref rather than out then it would have to
+            // be a class-level field so it could also be passed to BeginInvoke.
+            int threadId = 0;
+
+            // Call EndInvoke to retrieve the results.
+            string returnValue = caller.EndInvoke(out threadId, ar);
+
+            // Use the format string to format the output message.
+            Console.WriteLine(formatString, threadId, returnValue);
+        }
+
+    }
+}
+
